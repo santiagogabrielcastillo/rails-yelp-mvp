f13de51e9c6e55524e2f2f83fad3753127b10f1c
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-02-17 12:55:52 -0300

    reviews#new#create added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,9 +9,9 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    restaurant = Restaurant.new(restaurant_params)
-    if restaurant.save
-      redirect_to restaurant_path(restaurant)
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+
+  # GET /restaurants/restaurant_id/reviews/new
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end

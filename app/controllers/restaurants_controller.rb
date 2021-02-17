f3a1751907036a21afea8ffce166b70f3d41d260
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-02-17 12:38:26 -0300

    restaurants#show added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -17,10 +17,13 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
-
 end
